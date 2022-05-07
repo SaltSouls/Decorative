b591ce878ff0ceb8291d644feaf4eebd55bf51d1
SaltSouls <beyondtwinsouls@gmail.com>
2022-05-07 15:23:20 -0700

    Partial couch implementation

---
+++ src/main/java/com/teamabnormals/decorative/common/blocks/AbstractCouchBlock.java
@@ -0,0 +1,105 @@
+package com.teamabnormals.decorative.common.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import org.jetbrains.annotations.Nullable;
+
+public class AbstractCouchBlock extends Block implements SimpleWaterloggedBlock {
+
+    // Block Properties
+    public static final EnumProperty<CouchShape> SHAPE = EnumProperty.create("type", CouchShape.class);
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, WATERLOGGED, SHAPE);
+    }
+
+    public AbstractCouchBlock(BlockBehaviour.Properties properties) { super(properties); }
+
+    // Placement State of Block
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        BlockState state = super.getStateForPlacement(ctx);
+        return this.getCouchState(state, ctx.getLevel(), ctx.getClickedPos(), state.getValue(FACING));
+    }
+    private BlockState getCouchState(BlockState state, LevelAccessor world, BlockPos pos, Direction dir) {
+        boolean left = this.isCouch(world, pos, dir.getCounterClockWise(), dir) || this.isCouch(world, pos, dir.getCounterClockWise(), dir.getCounterClockWise());
+        boolean right = this.isCouch(world, pos, dir.getClockWise(), dir) || this.isCouch(world, pos, dir.getClockWise(), dir.getClockWise());
+        boolean cornerLeft = this.isCouch(world, pos, dir.getOpposite(), dir.getCounterClockWise());
+        boolean cornerRight = this.isCouch(world, pos, dir.getOpposite(), dir.getClockWise());
+
+        if(cornerLeft) { return state.setValue(SHAPE, CouchShape.CORNER_LEFT); }
+        else if(cornerRight) { return state.setValue(SHAPE, CouchShape.CORNER_RIGHT); }
+        else if(left && right) { return state.setValue(SHAPE, CouchShape.MIDDLE); }
+        else if(left) { return state.setValue(SHAPE, CouchShape.RIGHT); }
+        else if(right) { return state.setValue(SHAPE, CouchShape.LEFT); }
+        return state.setValue(SHAPE, CouchShape.SINGLE);
+    }
+    private boolean isCouch(LevelAccessor world, BlockPos pos, Direction direction, Direction targetDirection) {
+        BlockState state = world.getBlockState(pos.relative(direction));
+        if(state.getBlock() == this) {
+            Direction couchFacing = state.getValue(FACING);
+            return couchFacing.equals(targetDirection);
+        }
+        return false;
+    }
+    @Override
+    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
+        return this.getCouchState(state, world, currentPos, state.getValue(FACING));
+    }
+    // Rotation Properties
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        Direction direction = rotation.rotate(state.getValue(FACING));
+        return state.setValue(FACING, direction);
+    }
+    @Override
+    public BlockState mirror(BlockState state, Mirror rotation) {
+        Direction direction = rotation.mirror(state.getValue(FACING));
+        return state.setValue(FACING, direction);
+    }
+
+    //Waterlogged State
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false): super.getFluidState(state);
+    }
+
+    // Mob Pathfinding Conditions
+    @Override
+    public boolean isPathfindable(BlockState state, BlockGetter world, BlockPos pos, PathComputationType path) {
+        return false;
+    }
+
+    public enum CouchShape implements StringRepresentable {
+        SINGLE("single"),
+        LEFT("left"),
+        RIGHT("right"),
+        MIDDLE("middle"),
+        CORNER_LEFT("corner_left"),
+        CORNER_RIGHT("corner_right");
+
+        private final String id;
+        CouchShape(String id) { this.id = id; }
+        @Override
+        public String toString() { return id; }
+        @Override
+        public String getSerializedName() { return id; }
+    }
+}
--- src/main/java/com/teamabnormals/decorative/common/blocks/CouchBlock.java
+++ src/main/java/com/teamabnormals/decorative/common/blocks/CouchBlock.java
@@ -4,28 +4,24 @@ import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.teamabnormals.decorative.common.handlers.ShapeHandler;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.util.StringRepresentable;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CouchBlock extends Block implements SimpleWaterloggedBlock {
+public class CouchBlock extends AbstractCouchBlock {
     // Block Collision/Outline
     public final ImmutableMap<BlockState, VoxelShape> SHAPES;
+
+    public CouchBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false).setValue(SHAPE, CouchShape.SINGLE));
+        SHAPES = this.shapeConstructor(this.getStateDefinition().getPossibleStates());
+    }
+
     protected ImmutableBiMap<BlockState, VoxelShape> shapeConstructor(ImmutableList<BlockState> states) {
         Direction defaultFacing = Direction.NORTH.getOpposite();
         final VoxelShape[] SHAPE_BASE_SINGLE = ShapeHandler.getRotated(ShapeHandler.rotate(Block.box(1.0D, 0.0D, 1.0D, 15.0D, 6.0D, 16.0D), defaultFacing));
@@ -89,93 +85,6 @@ public class CouchBlock extends Block implements SimpleWaterloggedBlock {
             shape.put(state, ShapeHandler.combineAll(shapes));
         }
         return shape.build();
-    }
-
-    // Block Properties
-    public static final EnumProperty<CouchShape> SHAPE = EnumProperty.create("type", CouchShape.class);
-    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING, WATERLOGGED, SHAPE);
-    }
-
-    public CouchBlock(Properties properties) {
-        super(properties);
-        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false).setValue(SHAPE, CouchShape.SINGLE));
-        SHAPES = this.shapeConstructor(this.getStateDefinition().getPossibleStates());
-    }
-
-
-    // Placement State of Block
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-        BlockState state = super.getStateForPlacement(ctx);
-        return this.getCouchState(state, ctx.getLevel(), ctx.getClickedPos(), state.getValue(FACING));
-    }
-    private BlockState getCouchState(BlockState state, LevelAccessor world, BlockPos pos, Direction dir) {
-        boolean left = this.isCouch(world, pos, dir.getCounterClockWise(), dir) || this.isCouch(world, pos, dir.getCounterClockWise(), dir.getCounterClockWise());
-        boolean right = this.isCouch(world, pos, dir.getClockWise(), dir) || this.isCouch(world, pos, dir.getClockWise(), dir.getClockWise());
-        boolean cornerLeft = this.isCouch(world, pos, dir.getOpposite(), dir.getCounterClockWise());
-        boolean cornerRight = this.isCouch(world, pos, dir.getOpposite(), dir.getClockWise());
-
-        if(cornerLeft) { return state.setValue(SHAPE, CouchShape.CORNER_LEFT); }
-        else if(cornerRight) { return state.setValue(SHAPE, CouchShape.CORNER_RIGHT); }
-        else if(left && right) { return state.setValue(SHAPE, CouchShape.MIDDLE); }
-        else if(left) { return state.setValue(SHAPE, CouchShape.RIGHT); }
-        else if(right) { return state.setValue(SHAPE, CouchShape.LEFT); }
-        return state.setValue(SHAPE, CouchShape.SINGLE);
-    }
-    private boolean isCouch(LevelAccessor world, BlockPos pos, Direction direction, Direction targetDirection) {
-        BlockState state = world.getBlockState(pos.relative(direction));
-        if(state.getBlock() == this) {
-            Direction couchFacing = state.getValue(FACING);
-            return couchFacing.equals(targetDirection);
-        }
-        return false;
-    }
-    @Override
-    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
-        return this.getCouchState(state, world, currentPos, state.getValue(FACING));
-    }
-    // Rotation Properties
-    @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        Direction direction = rotation.rotate(state.getValue(FACING));
-        return state.setValue(FACING, direction);
-    }
-    @Override
-    public BlockState mirror(BlockState state, Mirror rotation) {
-        Direction direction = rotation.mirror(state.getValue(FACING));
-        return state.setValue(FACING, direction);
-    }
-
-    //Waterlogged State
-    @Override
-    public FluidState getFluidState(BlockState state) {
-        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false): super.getFluidState(state);
-    }
-
-    // Mob Pathfinding Conditions
-    @Override
-    public boolean isPathfindable(BlockState state, BlockGetter world, BlockPos pos, PathComputationType path) {
-        return false;
-    }
-
-    public enum CouchShape implements StringRepresentable {
-        SINGLE("single"),
-        LEFT("left"),
-        RIGHT("right"),
-        MIDDLE("middle"),
-        CORNER_LEFT("corner_left"),
-        CORNER_RIGHT("corner_right");
-
-        private final String id;
-        CouchShape(String id) { this.id = id; }
-        @Override
-        public String toString() { return id; }
-        @Override
-        public String getSerializedName() { return id; }
     }
 
 }
